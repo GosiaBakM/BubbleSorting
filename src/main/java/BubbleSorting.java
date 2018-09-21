2e6812ebe0f24c5e3f72c0e407fc93dab70324c4
GosiaBakM <machmalgorzata@gmail.com>
2018-09-21 22:47:37 +0200

    Add sorting method

--- src/main/java/BubbleSorting.java
+++ src/main/java/BubbleSorting.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class BubbleSorting {
     List<Integer> randomList;
 
-    public List<Integer> creatingRandomList(){
+    public List<Integer> creatingRandomList() {
         List<Integer> randomList = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -16,5 +16,19 @@ public class BubbleSorting {
         return randomList;
     }
 
+    public List<Integer> sorting() {
+        List<Integer> list = creatingRandomList();;
+        ex: for (int j = 0; j < list.size(); j++) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) > (list.get(i + 1))) {
+                    int replaece = list.get(i);
+                    list.add(i, list.get(i + 1));
+                    list.add(i + 1, replaece);
+                }
+                else break ex;
+            }
+        }
+        return list;
+    }
 
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
     public static void main(String [] args){
         BubbleSorting bubbleSorting = new BubbleSorting();
         System.out.println(bubbleSorting.creatingRandomList());
+        System.out.println(bubbleSorting.sorting());
     }
 }
